b78e96310bcfecbfcd559ecb822f2c07c60a07d0
musa.yazuju <musa.yazuju@musayazujuMacBook-Pro.local>
2023-11-12 16:43:46 +0900

    Move logic to ViewModel

--- app/src/main/java/com/musayazici/mealsapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/musayazici/mealsapp/ui/meals/MealsCategoriesViewModel.kt
@@ -1,13 +1,39 @@
 package com.musayazici.mealsapp.ui.meals
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.ViewModel
 import com.musayazici.mealsapp.model.MealsRepository
 import com.musayazici.mealsapp.model.response.MealResponse
 import com.musayazici.mealsapp.model.response.MealsCategoriesResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Callback
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()): ViewModel() {
-    suspend fun getMeals(): List<MealResponse> {
+
+    private val mealsJob = Job()
+    init {
+        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
+        scope.launch() {
+            val meals = getMeals()
+            mealsState.value = meals
+        }
+    }
+
+    val mealsState: MutableState<List<MealResponse>> = mutableStateOf(emptyList<MealResponse>())
+
+    override fun onCleared() {
+        super.onCleared()
+        mealsJob.cancel()
+    }
+
+    private suspend fun getMeals(): List<MealResponse> {
         return repository.getMeals().categories
     }
 }
